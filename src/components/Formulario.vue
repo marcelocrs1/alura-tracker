a1c0f1e39cf8d016d4ca5638c552c12fb2c221f1
Marcelo Santos <marcelocrs@gmail.com>
2023-01-04 17:18:46 +0000

    Continuar

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -2,7 +2,7 @@
   <div class="box formulario">
     <div class="columns">
       <div
-        class="column is-8"
+        class="column is-5"
         role="form"
         aria-label="Formulário para criação de uma nova tarefa"
       >
@@ -13,6 +13,20 @@
           v-model="descricao"
         />
       </div>
+      <div class="column is-3">
+        <div class="select">
+          <select v-model="idProjeto">
+            <option value="">Selecione o projeto</option>
+            <option
+              :value="projeto.id"
+              v-for="projeto in projetos"
+              :key="projeto.id"
+            >
+              {{ projeto.nome }}
+            </option>
+          </select>
+        </div>
+      </div>
       <div class="column">
         <Temporizador @aoTemporizadorFinalizado="finalizarTarefa" />
       </div>
@@ -21,8 +35,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
 import Temporizador from "./Temporizador.vue";
+import { useStore } from "vuex";
+import { key } from "@/store";
 
 export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
@@ -34,18 +50,24 @@ export default defineComponent({
   data() {
     return {
       descricao: "",
+      idProjeto: "",
     };
   },
   methods: {
     finalizarTarefa(tempoDecorrido: number): void {
-      console.log("Tempo da Tarefa", tempoDecorrido);
-      console.log("Descrição da Tarefa", this.descricao);
       this.$emit("aoSalvarTarefa", {
         duracaoEmSegundos: tempoDecorrido,
         descricao: this.descricao,
+        projeto: this.projetos.find((proj) => proj.id == this.idProjeto),
       });
       this.descricao = "";
     },
+  },
+  setup() {
+    const store = useStore(key);
+    return {
+      projetos: computed(() => store.state.projetos),
+    };
   },
 });
 </script>
